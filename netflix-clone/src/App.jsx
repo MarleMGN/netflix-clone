9be111a6f08047bb717a0fe6c3a6d5c41bcb24d1
MarleMGN <m0b.torres23@gmail.com>
2026-02-06 12:42:14 -0800

    asjdaksd

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import Home from './pages/Home/Home.jsx'
+import React from "react";
+import Home from "./pages/Home/Home.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login.jsx";
 
 const App = () => {
   return (
     <div>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
